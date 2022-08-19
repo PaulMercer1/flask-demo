25588f10027e93d132f3efb7fc00c43ef55c7c26
Paul Mercer <paul.mercer@qa.com>
2022-08-19 11:49:47 +0100

    Add createdb route

--- application/routes.py
+++ application/routes.py
@@ -1,3 +1,4 @@
+from time import sleep
 from flask import redirect, url_for, render_template, request
 
 from application import app, db
@@ -21,3 +22,15 @@ def add():
 
     error = next(iter(form.name.errors), None)
     return redirect(url_for('index', error=error))
+
+@app.route('/createdb', methods=['GET'])
+def createdb():
+    db.create_all()
+    tries = 5
+    while tries > 0:
+        try:        
+            db.create_all()
+            tries = 0
+        except Exception as ex:
+            tries-= 1
+            sleep(5)
